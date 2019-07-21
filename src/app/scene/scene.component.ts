c236b7d26e3cbd362d3cf853d27d779fad4edd55
finedjentleman <rawatabhi1729@gmail.com>
2019-07-22 02:54:23 +0530

    Added texture to the cube

--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -9,23 +9,26 @@ import * as THREE from 'three';
 })
 export class SceneComponent implements OnInit {
 
-  private loader;
-
   private camera: THREE.PerspectiveCamera;
   private scene: THREE.Scene;
   private renderer: THREE.WebGLRenderer;;
 
   private geometry: THREE.BoxGeometry; //local
+  
   private material: THREE.MeshBasicMaterial; //local
+  private texture: THREE.Texture;
+  // ! texture is of type THREE.Texture, not THREE.TextureLoader
+  
   private cube: THREE.Mesh;
 
   constructor() { }
 
   ngOnInit() {
-    //this.loader = new THREE.ColladaLoader();
     
     window.addEventListener('resize', this.onWindowResize.bind(this), false);
-
+    // so that the component renders well with browser window resize
+    // ! use this.onWindowResize.bind(this) instead of this.onWindowResize()
+    
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(
       75,
@@ -34,14 +37,17 @@ export class SceneComponent implements OnInit {
       1000
     );
 
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer = new THREE.WebGLRenderer({ antialias: true }); // antialias added for sharp edges
 
     this.renderer.setSize(window.innerWidth, window.innerHeight);
 
     document.body.appendChild(this.renderer.domElement);
 
     this.geometry = new THREE.BoxGeometry(2, 2, 2);
-    this.material = new THREE.MeshBasicMaterial({ color: 0x0000ff});
+    
+    this.texture = new THREE.TextureLoader().load('assets/crate.gif');
+    this.material = new THREE.MeshBasicMaterial({ map: this.texture });
+
     this.cube = new THREE.Mesh(this.geometry, this.material);
     this.scene.add(this.cube);
   
@@ -52,6 +58,7 @@ export class SceneComponent implements OnInit {
 
   animate(){
     requestAnimationFrame(this.animate.bind(this));
+    // ! use this.animate.bind(this) instead of this.animate()
 
     this.cube.rotation.x += 0.01;
     this.cube.rotation.y += 0.01;
